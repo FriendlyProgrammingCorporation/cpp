54c6eeb91d03d224bff6dc077719c4fa28e71943
phen0menon <garaganda1@mail.ru>
2016-01-24 14:30:41 +0400

    First console mini-game on cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,7 +1,7 @@
 /*
-	name : Phenomenon
-	github : phen0menon
-	language : cpp
+name : Phenomenon
+github : phen0menon
+language : cpp
 */
 
 #include <iostream>
@@ -9,164 +9,168 @@
 #include <iomanip>
 #include <time.h>
 
+char matrix[3][3];
+int row = 0; // just var
+int col = 0; // just var
+const int rows = 3; // for matrix
+const int cols = 3; // for matrix
+bool playerOne = true; // ' x '
+bool playerTwo = false; // ' 0 ' 
+bool isPlay = true; 
+char* alph = "1234567890-=\`qwert'yuiop[]asdfghjkl;zxcvbnm,./יצףךוםדרשחץתפûגאןנמכהז‎ÿקסלטעü‏.!¹;%:?*()_+";
 
-int main() {
-	srand(time(nullptr));
-	setlocale(LC_ALL, "Russian");
-	int row = 0; // just statement
-	int col = 0; // just statement
-	const int rows = 3; // for matrix
-	const int cols = 3; // for matrix
-	bool gameIsOver = false; // if not 
-	bool playerOne = true; // ' x '
-	bool playerTwo = false; // ' 0 ' 
+void ReadME() {
+	char choice = 0;
+	do {
+		std::cout << ".............. Welcome to TicTacToe game! ............." << std::endl;
+		std::cout << ".................. Rules are simple ..................." << std::endl;
+		std::cout << "|1| You can move only (1, 1) to (3, 3), same as in geometry" << std::endl;
+		std::cout << "|2| You can't move to point that already used" << std::endl;
+		std::cout << "|3| Mercy me for my English :D :D :D" << std::endl;
+		std::cout << "------------------------------------------------------------" << std::endl;
+		std::cout << "                  TO START PRESS ANY KEY                 " << std::endl;
+		choice = _getch();
 
-	/* * * * * * * * * * * * * * * * * * * * * * * *
-	* * * * * * initialize char *matrix * * * * * */
-	
-	char **matrix = new char*[rows];
-	for (int i = 0; i < rows; i++) {
-		matrix[i] = new char[cols];
+	} while (!strchr (alph, choice));
+	system("cls");
+}
+
+void win() {
+	isPlay = false;
+	playerOne = false;
+	playerTwo = false;
+}
+
+void fillMatrix() {
+	for (int i(0); i < 3; i++) {
+		for (int j(0); j < 3; j++) {
+			matrix[i][j] = '-';
+		}
 	}
-	for (int i = 0; i < rows; i++) {
+}
+
+void showMatrix() {
+	for (int i(0); i < 3; i++) {
 		std::cout << " | ";
-		for (int j = 0; j < cols; j++) {
-			matrix[i][j] = 'n';
+		for (int j(0); j < 3; j++) {
 			std::cout << matrix[i][j] << " ";
 		}
 		std::cout << " | " << std::endl;
 	}
-
-	/* * * * * * * * * * * * * * * * * * * * * * * *
-	* * * * * * player choice to moving * * * * * */
-	while (true) {
+}
+void play() {
+	while (isPlay) {
 		if (playerOne) {
+			bool problems = false;
+			bool fill = false;
+			std::cout << " ------------------------------ ";
 			std::cout << std::endl << std::setw(4) << " PLAYER 1 IS MOVING " << std::endl;
 			do {
-				std::cout << "enter your move: "; // 0, 0 to 9, 9
-				std::cin >> row;
-				std::cin >> col;
-				if (!(row >= 0 && row <= 3 && col >= 0 && col <= 3)) {
-					std::cout << "enter legal move!" << std::endl;
-				}
-			} while (!(row >= 0 && row <= 3 && col >= 0 && col <= 3));
-			matrix[row - 1][col - 1] = 'x';
-
-			if (matrix[0][0] && matrix[0][1] && matrix[0][2] == 'n') { // 00 01 02
-				std::cout << "\n----PLAYER 1 WINS----" << std::endl;
-				break;
-			}
-			if (matrix[0][0] && matrix[1][0] && matrix[2][0] == 'n') { // 00 10 20
-				std::cout << "\n----PLAYER 1 WINS----" << std::endl;
-				break;
-			}
-			if (matrix[0][0] && matrix[1][1] && matrix[2][2] == 'n') { // 00 11 22
-				std::cout << "\n----PLAYER 1 WINS----" << std::endl;
-				break;
-			}
-			if (matrix[1][0] && matrix[1][1] && matrix[1][2] == 'n') { // 10 11 12
-				std::cout << "\n----PLAYER 1 WINS----" << std::endl;
-				break;
-			}
-			if (matrix[0][1] && matrix[1][1] && matrix[2][1] == 'n') { // 01 11 21
-				std::cout << "\n----PLAYER 1 WINS----" << std::endl;
-				break;
-			}
-			if (matrix[2][0] && matrix[1][1] && matrix[0][2] == 'n') { // 20 11 02
-				std::cout << "\n----PLAYER 1 WINS----" << std::endl;
-				break;
-			}
-			if (matrix[0][2] && matrix[1][2] && matrix[2][2] == 'n') { // 02 12 22
-				std::cout << "\n----PLAYER 1 WINS----" << std::endl;
-				break;
-			}
-			playerOne = false;
-			playerTwo = true;
-			for (int i = 0; i < rows; i++) {
-				std::cout << " | ";
-				for (int j = 0; j < cols; j++) {
-					std::cout << matrix[i][j] << " ";
-				}
-				std::cout << " | " << std::endl;
-			}
-		}
-		if (playerTwo) {
-			std::cout << std::endl << std::setw(4) << " PLAYER 2 IS MOVING " << std::endl;
-			do {
+				problems = false;
+				fill = false;
 				std::cout << "enter your move: "; // 0, 0 to 3, 3
 				std::cin >> row;
 				std::cin >> col;
 				if (!(row >= 0 && row <= 3 && col >= 0 && col <= 3)) {
 					std::cout << "enter legal move!" << std::endl;
 				}
-			} while (!(row >= 0 && row <= 3 && col >= 0 && col <= 3));
-			matrix[row - 1][col - 1] = '0';
-
-			if (matrix[0][0] && matrix[0][1] && matrix[0][2] == '0') { // 00 01 02
-				std::cout << "\n----PLAYER 2 WINS----" << std::endl;
-				break;
-			} 
-			if (matrix[0][0] && matrix[1][0] && matrix[2][0] == '0') { // 00 10 20
-				std::cout << "\n----PLAYER 2 WINS----" << std::endl;
-				break;
-			}
-			if (matrix[0][0] && matrix[1][1] && matrix[2][2] == '0') { // 00 11 22
-				std::cout << "\n----PLAYER 2 WINS----" << std::endl;
-				break;
-			}
-			if (matrix[1][0] && matrix[1][1] && matrix[1][2] == '0') { // 10 11 12
-				std::cout << "\n----PLAYER 2 WINS----" << std::endl;
-				break;
-			}
-			if (matrix[0][1] && matrix[1][1] && matrix[2][1] == '0') { // 01 11 21
-				std::cout << "\n----PLAYER 2 WINS----" << std::endl;
-				break;
-			}
-			if (matrix[2][0] && matrix[1][1] && matrix[0][2] == '0') { // 20 11 02
-				std::cout << "\n----PLAYER 2 WINS----" << std::endl;
-				break;
-			}
-			if (matrix[0][2] && matrix[1][2] && matrix[2][2] == '0') { // 02 12 22
-				std::cout << "\n----PLAYER 2 WINS----" << std::endl;
-				break;
-			}
-			
-			playerOne = false;
-			playerOne = true;
-			for (int i = 0; i < rows; i++) {
-				std::cout << " | ";
-				for (int j = 0; j < cols; j++) {
-					std::cout << matrix[i][j] << " ";
+				if ((matrix[row - 1][col - 1] == 'x') || (matrix[row - 1][col - 1] == '0')) {
+					std::cout << "OOPS! " << row << "." << col << " already taken. " << std::endl;
+					problems = true;
 				}
-				std::cout << " | " << std::endl;
+					
+				
+				if (problems == false) {
+					matrix[row - 1][col - 1] = 'x'; // fill ' x '
+					fill = true;
+				} 
+			} while (!((row >= 0 && row <= 3 && col >= 0 && col <= 3) && (fill)));
+			
+			//while (!(((row >= 0 && row <= 3 && col >= 0 && col <= 3))
+			//	&& ((matrix[row - 1][col - 1] == 'x'))));
+
+			//matrix[row - 1][col - 1] = 'x'; // fill ' X '
+
+			showMatrix();
+
+			playerOne = !playerOne;
+			playerTwo = !playerTwo;;
+			for (int i = 0; i < rows; i++) {
+				if (((matrix[i][0] == 'x') && (matrix[i][1] == 'x') && (matrix[i][2] == 'x'))) {
+					std::cout << " ... PLAYER 1 WINS ... " << std::endl;
+					win();
+				}
+				else
+					if (((matrix[0][i] == 'x') && (matrix[1][i] == 'x') && (matrix[2][i] == 'x'))) {
+						std::cout << " ... PLAYER 1 WINS ... " << std::endl;
+						win();
+					}
+					else if (((matrix[i][i] == 'x') && (matrix[i + 1][i + 1] == 'x') && (matrix[i + 2][i + 2] == 'x'))
+						|| ((matrix[i][2] == 'x') && (matrix[i + 1][1] == 'x') && (matrix[i + 2][0] == 'x'))) {
+						std::cout << " ... PLAYER 1 WINS ... " << std::endl;
+						win();
+					}
 			}
 		}
-		std::cout << std::endl;
-	}
+		if (playerTwo) {
+			bool problems = false;
+			bool fill = false;
+			std::cout << " ------------------------------ ";
+			std::cout << std::endl << " PLAYER 2 IS MOVING " << std::endl;
+			do {
+				problems = false;
+				fill = false;
+				std::cout << "enter your move: "; // 0, 0 to 3, 3
+				std::cin >> row;
+				std::cin >> col;
+				if (!(row >= 0 && row <= 3 && col >= 0 && col <= 3)) {
+					std::cout << "enter legal move!" << std::endl;
+				}
+				if ((matrix[row - 1][col - 1] == 'x') || (matrix[row - 1][col - 1] == '0')) {
+					std::cout << "OOPS! " << row << "." << col << " already taken. " << std::endl;
+					problems = true;
+				}
 
-	/* --------------------- E X A M P L E S ---------------------- 
-	* if(matrix[0][0] && matrix[0][1] && matrix[0][2]) win 00 01 02
-	* if(matrix[0][0] && matrix[1][0] && matrix[2][0]) win 00 10 20
-	* if(matrix[0][0] && matrix[1][1] && matrix[2][2]) win 00 11 22
-	* if(matrix[1][0] && matrix[1][1] && matrix[1][2]) win 10 11 12
-	* if(matrix[0][1] && matrix[1][1] && matrix[2][1]) win 01 11 21
-	* if(matrix[2][0] && matrix[1][1] && matrix[0][2]) win 20 11 02
-	* if(matrix[0][2] && matrix[1][2] && matrix[2][2]) win 02 12 22
-	* row 1, col 1
-	* matrix[row-1][col-1] = 'x'; 
-	--------------------------------------------------------------*/
 
+				if (problems == false) {
+					matrix[row - 1][col - 1] = '0'; // fill ' x '
+					fill = true;
+				}
+			} while (!((row >= 0 && row <= 3 && col >= 0 && col <= 3) && (fill)));
 
-	for (int i = 0; i < rows; i++) {
-		std::cout << " | ";
-		for (int j = 0; j < cols; j++) {
-			std::cout << matrix[i][j] << " ";
+			//matrix[row - 1][col - 1] = '0'; // fill ' 0 '
+
+			showMatrix();
+
+			for (int i = 0; i < rows; i++) {
+				if (((matrix[i][0] == '0') && (matrix[i][1] == '0') && (matrix[i][2] == '0'))) {
+					std::cout << " ... PLAYER 2 WINS ... " << std::endl;
+					win();
+				}
+				else
+					if (((matrix[0][i] ==  '0') && (matrix[1][i] == '0') && (matrix[2][i] == '0'))) {
+						std::cout << " ... PLAYER 2 WINS ... " << std::endl;
+						win();
+					}
+					else if (((matrix[i][i] == '0') && (matrix[i + 1][i + 1] == '0') && (matrix[i + 2][i + 2] == '0'))
+						|| ((matrix[i][2] == '0') && (matrix[i + 1][1] == '0') && (matrix[i + 2][0] == '0'))) {
+						std::cout << " ... PLAYER 1 WINS ... " << std::endl;
+						win();
+					}
+			}
+			playerTwo = !playerTwo;
+			playerOne = !playerOne;
 		}
-		std::cout << " | " << std::endl;
 	}
+}
 
-	/* * * * * * * * * * * * * * * * * * * * * * * * */
-	delete [] matrix;
+int main() {
+	srand(time(nullptr));
+	setlocale(LC_ALL, "Russian");
+	ReadME();
+	fillMatrix(); // fill in matrix
+	showMatrix(); // show full matrix
+	play();
 	_getch();
 }
 
